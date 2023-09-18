7da6310c837d4f36a2fedf8fe9ba2b8ef6eea852
Omar Badr <omarbadrh.ob@gmail.com>
2023-09-18 15:09:00 +0200

    :shirt: Adding linting and prettier to front-end

--- front-end/.eslintrc.cjs
+++ front-end/.eslintrc.cjs
@@ -2,17 +2,26 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
+    "airbnb",
+    'airbnb-typescript',
+    "airbnb/hooks",
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './front-end/tsconfig.json',
+  },
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react', '@typescript-eslint', 'prettier',],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/react-in-jsx-scope': 0,
   },
 }
